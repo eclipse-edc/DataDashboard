6795d89101abaaa19503acdc58de0d568308b424
akruk <kruk.adam@web.de>
2023-07-26 11:58:05 +0200

    mock data from json file

--- src/modules/edc-demo/components/catalog-browser-confirmation/catalog-browser-confirmation.component.ts
+++ src/modules/edc-demo/components/catalog-browser-confirmation/catalog-browser-confirmation.component.ts
@@ -1,9 +1,15 @@
 import {Component} from '@angular/core';
+import data from '../../../../data.json';
 
 @Component({
   selector: 'edc-demo-introduction',
   templateUrl: './catalog-browser-confirmation.component.html',
   styleUrls: ['./catalog-browser-confirmation.component.scss']
 })
+
 export class CatalogBrowserConfirmation {
+  id = data[0].id;
+  provider = data[0].provider;
+  start = data[0].contractStart;
+  consumer = data[0].consumer;
 }
