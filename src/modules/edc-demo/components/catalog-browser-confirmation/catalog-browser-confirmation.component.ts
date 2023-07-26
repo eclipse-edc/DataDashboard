5a963ba3e5efae693a787bb5d5c5a4f3499ffb28
akruk <kruk.adam@web.de>
2023-07-26 12:29:18 +0200

    api preperation

--- src/modules/edc-demo/components/catalog-browser-confirmation/catalog-browser-confirmation.component.ts
+++ src/modules/edc-demo/components/catalog-browser-confirmation/catalog-browser-confirmation.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import data from '../../../../data.json';
+import { PostService } from './post.service';
+
 
 @Component({
   selector: 'edc-demo-introduction',
@@ -7,9 +9,20 @@ import data from '../../../../data.json';
   styleUrls: ['./catalog-browser-confirmation.component.scss']
 })
 
-export class CatalogBrowserConfirmation {
+export class CatalogBrowserConfirmation implements OnInit{
   id = data[0].id;
   provider = data[0].provider;
   start = data[0].contractStart;
   consumer = data[0].consumer;
+
+  posts:any;
+  
+  constructor(private service:PostService) {}
+  
+  ngOnInit() {
+      this.service.getPosts()
+        .subscribe(response => {
+          this.posts = response;
+        });
+  } 
 }
