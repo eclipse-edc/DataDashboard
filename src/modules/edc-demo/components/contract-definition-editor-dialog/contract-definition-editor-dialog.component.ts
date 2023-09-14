489b3a06864f1730dc14bdcbbbc49b67066b3511
Jan Peter Meyer <102802985+janpmeyer@users.noreply.github.com>
2023-09-14 10:17:38 +0200

    feat: connector client: switch PolicyService (#90)
    * feat: switch PolicyDefinition to connector client (#38)
    
    * feat: fix id and permission on creating new Policy (#58)
    
    * feat: use get functions for obligations and prohibition in View (#58)
    
    * feat: remove unnecessary import (#58)

--- src/modules/edc-demo/components/contract-definition-editor-dialog/contract-definition-editor-dialog.component.ts
+++ src/modules/edc-demo/components/contract-definition-editor-dialog/contract-definition-editor-dialog.component.ts
@@ -1,9 +1,9 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {
-  AssetService, PolicyDefinitionResponseDto, PolicyService
+  AssetService, PolicyService
 } from "../../../mgmt-api-client";
-import { Asset, ContractDefinitionInput } from "../../../mgmt-api-client/model"
+import { Asset, ContractDefinitionInput, PolicyDefinition } from "../../../mgmt-api-client/model"
 
 
 @Component({
@@ -13,12 +13,12 @@ import { Asset, ContractDefinitionInput } from "../../../mgmt-api-client/model"
 })
 export class ContractDefinitionEditorDialog implements OnInit {
 
-  policies: Array<PolicyDefinitionResponseDto> = [];
+  policies: Array<PolicyDefinition> = [];
   availableAssets: Asset[] = [];
   name: string = '';
   editMode = false;
-  accessPolicy?: PolicyDefinitionResponseDto;
-  contractPolicy?: PolicyDefinitionResponseDto;
+  accessPolicy?: PolicyDefinition;
+  contractPolicy?: PolicyDefinition;
   assets: Asset[] = [];
   contractDefinition: ContractDefinitionInput = {
     "@id": '',
--- src/modules/mgmt-api-client/model/edc-connector-entities.ts
+++ src/modules/mgmt-api-client/model/edc-connector-entities.ts
@@ -25,6 +25,7 @@ export type {
   Policy,
   PolicyDefinition,
   PolicyDefinitionInput,
+  PolicyInput,
   PolicyType,
   Prohibition,
   QuerySpec,
